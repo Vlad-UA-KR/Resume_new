66550d4a2b6db49beeb0bbacc2720e2c39126e23
Vlad-UA-KR <vlad.fedorchenkog@gmail.com>
2022-09-29 16:13:03 +0300

    add new section

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,16 @@ $(function () {
 		prevArrow: '<button class="btn btn__left""><svg class="prevArr" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></button>'
 	})
 })
+
+$(function () {
+	$('.work-slider').slick({
+		slidesToShow: 1,
+		speed: 500,
+		arrows: false,
+		dots: true,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 2500,
+		variabgleWidth: true,
+	})
+})
